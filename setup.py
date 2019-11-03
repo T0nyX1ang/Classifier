cd99bc1cb38b8730b5654ad12a95b0347c2f36f1
Tony Xiang <33901373+T0nyX1ang@users.noreply.github.com>
2019-11-03 22:08:47 +0800

    Python package generation

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+setup(
+	name='Classifier',
+	version='0.1.0',
+	keywords=['File Management', 'Classification'],
+	packages=find_packages(),
+	description='A simple classifier to manage your messy desktop files.',
+	author='Tony Xiang',
+	author_email='tonyxfy[#]gmail.com',
+	url='https://github.com/T0nyX1ang/Classifier',
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'Programming Language :: Python :: 3.5 :: 3.6 :: 3.7 :: 3.8',
+		'Environment :: Console',
+		'License :: OSI Approved :: MIT License',
+		"Operating System :: OS Independent"
+	],
+	platforms='all',
+	entry_points={
+		'console_scripts': [
+			'config=Classifier:config',
+			'classify=Classifier:classify',
+			'merge=Classifier:merge',
+		]
+	}
+)
